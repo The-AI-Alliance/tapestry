96396fa7e81e414b9a3d2eb896047249c851d893
Jesse <jesseneumann@gmail.com>
2026-06-13 12:46:11 -0600

    feat: add consortium experiment metrics runner
    Signed-off-by: Jesse <jesseneumann@gmail.com>

---
+++ contrib/jneums-consortium-experiment/consortium_experiment/__init__.py
@@ -0,0 +1,21 @@
+"""Contrib experiment tooling for the Tapestry consortium-training PoC."""
+
+from .experiment import (
+    ConsortiumExperimentRunner,
+    ExperimentConfig,
+    ExperimentResult,
+    ExperimentSummary,
+    NodeSpec,
+    PolicySpec,
+    RoundMetrics,
+)
+
+__all__ = [
+    "ConsortiumExperimentRunner",
+    "ExperimentConfig",
+    "ExperimentResult",
+    "ExperimentSummary",
+    "NodeSpec",
+    "PolicySpec",
+    "RoundMetrics",
+]
